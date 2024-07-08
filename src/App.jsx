7b2a92965ea30f436c230f40e8ca52735bba0be4
idlakshan <id.lakshan21@gmail.com>
2024-07-08 16:25:08 +0530

    feat: create navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <nav className="navbar">
+        <div className="navgroup">
+          <a className="navbar-brand" to="/">StoreHub</a>
+          <div className="nav-links">
+            <a href="/">Home</a>
+            <a href="/shop">Shop</a>
+          </div>
+        </div>
+        <div className="cart-section">
+          <div>
+            <a href="/cart" className="cart-link">
+              <p className="cart-count">{0}</p>
+              <div className="cart-icon">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-shopping-cart"><circle cx="8" cy="21" r="1" /><circle cx="19" cy="21" r="1" /><path d="M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12" /></svg>
+                Cart
+              </div>
+            </a>
+          </div>
+          <p>Hi, Dimuthu</p>
+        </div>
+      </nav>
+    </div>
   )
 }
 
