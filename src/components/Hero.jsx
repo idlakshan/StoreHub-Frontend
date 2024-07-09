df12a79e011f354470cc52a87ec351d13f672ae7
idlakshan <id.lakshan21@gmail.com>
2024-07-09 22:05:42 +0530

    refactor: replace CSS with Tailwind CSS

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <section className="hero-section">
-    <div className="hero-container">
-      <div className="hero-content">
-        <span className="discount-badge">WEEKLY DISCOUNT</span>
-        <h1 className="hero-title">
+    <section className="py-8 px-16">
+    <div className="grid grid-cols-2 rounded-md min-h-[60vh] bg-[#f4f8f9]">
+      <div className="flex flex-col justify-center p-16 gap-y-4">
+        <span className="block rounded-full px-2 py-1 text-xs  w-fit bg-[#febc26]">WEEKLY DISCOUNT</span>
+        <h1 className="text-6xl font-semibold">
           Premium Product Online Shop
         </h1>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quos suscipit est autem quia? Voluptatem?
         </p>
-        <a href="/shop" className="shop-now-btn">Shop Now</a>
+        <a href="/shop" className="px-4 py-2 text-white font-medium bg-black rounded w-fit">Shop Now</a>
       </div>
-      <div className="hero-image-container">
-        <img src="https://fee-storefront.vercel.app/assets/hero/hero.jpg" alt="" className="hero-image" />
+      <div className="relative">
+        <img src="https://fee-storefront.vercel.app/assets/hero/hero.jpg" alt="" className="w-full h-full object-cover" />
       </div>
     </div>
   </section>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,19 +2,19 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-        <div className="navgroup">
-          <a className="navbar-brand" to="/">StoreHub</a>
-          <div className="nav-links">
+    <nav className="flex items-center justify-between py-8 px-16">
+        <div className="flex items-center gap-x-16">
+          <a className="font-semibold text-3xl" to="/">StoreHub</a>
+          <div className="flex items-center gap-x-4">
             <a href="/">Home</a>
             <a href="/shop">Shop</a>
           </div>
         </div>
-        <div className="cart-section">
+        <div className="flex items-center gap-x-8">
           <div>
-            <a href="/cart" className="cart-link">
-              <p className="cart-count">{0}</p>
-              <div className="cart-icon">
+            <a href="/cart"  className="flex items-center gap-x-4 relative">
+              <p className="text-lg">{0}</p>
+              <div className="flex items-center gap-x-2">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-shopping-cart"><circle cx="8" cy="21" r="1" /><circle cx="19" cy="21" r="1" /><path d="M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12" /></svg>
                 Cart
               </div>
